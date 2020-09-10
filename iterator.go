871a771d4f262db1480154aaa510eaa7696c5189
Levi Aul <levi@leviaul.com>
2020-09-10 10:44:04 -0700

    Make iterator API non-enclosing

--- iterator.go
+++ iterator.go
@@ -1,95 +1,144 @@
 package llrb
 
-type ItemIterator func(i Item) bool
+import "container/list"
 
-//func (t *Tree) Ascend(iterator ItemIterator) {
-//	t.AscendGreaterOrEqual(Inf(-1), iterator)
-//}
+type filterSides byte
 
-func (t *LLRB) AscendRange(greaterOrEqual, lessThan Item, iterator ItemIterator) {
-	t.ascendRange(t.root, greaterOrEqual, lessThan, iterator)
+const (
+	filterSideAt    filterSides = 1
+	filterSideBelow             = 2
+	filterSideAbove             = 4
+)
+
+type filterFunc func(item Item) filterSides
+type enqueueFunc func(queue *list.List, pos *Node)
+
+type Iterator struct {
+	walkQueue *list.List
+	enqueueF  enqueueFunc
 }
 
-func (t *LLRB) ascendRange(h *Node, inf, sup Item, iterator ItemIterator) bool {
-	if h == nil {
-		return true
-	}
-	if !less(h.Item, sup) {
-		return t.ascendRange(h.Left, inf, sup, iterator)
-	}
-	if less(h.Item, inf) {
-		return t.ascendRange(h.Right, inf, sup, iterator)
-	}
+func newIterator(root *Node, enqueueF enqueueFunc) *Iterator {
+	walkQueue := list.New()
+	walkQueue.PushFront(root)
 
-	if !t.ascendRange(h.Left, inf, sup, iterator) {
-		return false
+	return &Iterator{
+		walkQueue: walkQueue,
+		enqueueF:  enqueueF,
 	}
-	if !iterator(h.Item) {
-		return false
-	}
-	return t.ascendRange(h.Right, inf, sup, iterator)
 }
 
-// AscendGreaterOrEqual will call iterator once for each element greater or equal to
-// pivot in ascending order. It will stop whenever the iterator returns false.
-func (t *LLRB) AscendGreaterOrEqual(pivot Item, iterator ItemIterator) {
-	t.ascendGreaterOrEqual(t.root, pivot, iterator)
-}
-
-func (t *LLRB) ascendGreaterOrEqual(h *Node, pivot Item, iterator ItemIterator) bool {
-	if h == nil {
-		return true
+func (it *Iterator) Read() Item {
+	if it.walkQueue == nil {
+		return nil
 	}
-	if !less(h.Item, pivot) {
-		if !t.ascendGreaterOrEqual(h.Left, pivot, iterator) {
-			return false
+
+	var pos *list.Element
+
+	for {
+		pos = it.walkQueue.Front()
+		if pos == nil {
+			it.walkQueue = nil
+			return nil
 		}
-		if !iterator(h.Item) {
-			return false
-		}
-	}
-	return t.ascendGreaterOrEqual(h.Right, pivot, iterator)
-}
+		it.walkQueue.Remove(pos)
 
-// AscendLessThan will call iterator once for each element lower than
-// pivot in ascending order. It will stop whenever the iterator returns false.
-func (t *LLRB) AscendLessThan(pivot Item, iterator ItemIterator) {
-	t.ascendLessThan(t.root, pivot, iterator)
-}
-
-func (t *LLRB) ascendLessThan(h *Node, pivot Item, iterator ItemIterator) bool {
-	if h == nil {
-		return true
-	}
-	if !t.ascendLessThan(h.Left, pivot, iterator) {
-		return false
-	}
-	if less(h.Item, pivot) {
-		if !iterator(h.Item) {
-			return false
-		}
-		return t.ascendLessThan(h.Right, pivot, iterator)
-	}
-	return true
-}
-
-// DescendLessOrEqual will call iterator once for each element less than the
-// pivot in descending order. It will stop whenever the iterator returns false.
-func (t *LLRB) DescendLessOrEqual(pivot Item, iterator ItemIterator) {
-	t.descendLessOrEqual(t.root, pivot, iterator)
-}
-
-func (t *LLRB) descendLessOrEqual(h *Node, pivot Item, iterator ItemIterator) bool {
-	if h == nil {
-		return true
-	}
-	if less(h.Item, pivot) || !less(pivot, h.Item) {
-		if !t.descendLessOrEqual(h.Right, pivot, iterator) {
-			return false
-		}
-		if !iterator(h.Item) {
-			return false
+		switch posV := pos.Value.(type) {
+		case *Node:
+			it.enqueueF(it.walkQueue, posV)
+		case Item:
+			return posV
 		}
 	}
-	return t.descendLessOrEqual(h.Left, pivot, iterator)
 }
+
+func (t *LLRB) ascendF(filterF filterFunc) *Iterator {
+	enqueueF := func(q *list.List, pos *Node) {
+		filterSides := filterF(pos.Item)
+		if pos.Right != nil && (filterSides&filterSideAbove) > 0 {
+			q.PushFront(pos.Right)
+		}
+		if (filterSides & filterSideAt) > 0 {
+			q.PushFront(pos.Item)
+		}
+		if pos.Left != nil && (filterSides&filterSideBelow) > 0 {
+			q.PushFront(pos.Left)
+		}
+	}
+
+	return newIterator(t.root, enqueueF)
+}
+
+func (t *LLRB) descendF(filterF filterFunc) *Iterator {
+	enqueueF := func(q *list.List, pos *Node) {
+		filterSides := filterF(pos.Item)
+		if pos.Left != nil && (filterSides&filterSideBelow) > 0 {
+			q.PushFront(pos.Left)
+		}
+		if (filterSides & filterSideAt) > 0 {
+			q.PushFront(pos.Item)
+		}
+		if pos.Right != nil && (filterSides&filterSideAbove) > 0 {
+			q.PushFront(pos.Right)
+		}
+	}
+
+	return newIterator(t.root, enqueueF)
+}
+
+var filterSideAny = filterSideBelow | filterSideAt | filterSideAbove
+
+func filterAny(item Item) filterSides {
+	return filterSideAny
+}
+
+func filterAbove(pivot Item) filterFunc {
+	return func(item Item) filterSides {
+		if !less(pivot, item) {
+			return filterSideAbove
+		}
+		return filterSideAny
+	}
+}
+
+func filterAtOrAbove(pivot Item) filterFunc {
+	return func(item Item) filterSides {
+		if less(item, pivot) {
+			return filterSideAbove
+		}
+		return filterSideAny
+	}
+}
+
+func filterBelow(pivot Item) filterFunc {
+	return func(item Item) filterSides {
+		if !less(item, pivot) {
+			return filterSideBelow
+		}
+		return filterSideAny
+	}
+}
+
+func filterAtOrBelow(pivot Item) filterFunc {
+	return func(item Item) filterSides {
+		if less(pivot, item) {
+			return filterSideBelow
+		}
+		return filterSideAny
+	}
+}
+
+func (t *LLRB) Ascend() *Iterator  { return t.ascendF(filterAny) }
+func (t *LLRB) Descend() *Iterator { return t.descendF(filterAny) }
+
+func (t *LLRB) AscendAbove(pivot Item) *Iterator  { return t.ascendF(filterAbove(pivot)) }
+func (t *LLRB) DescendAbove(pivot Item) *Iterator { return t.descendF(filterAbove(pivot)) }
+
+func (t *LLRB) AscendAtOrAbove(pivot Item) *Iterator  { return t.ascendF(filterAtOrAbove(pivot)) }
+func (t *LLRB) DescendAtOrAbove(pivot Item) *Iterator { return t.descendF(filterAtOrAbove(pivot)) }
+
+func (t *LLRB) AscendBelow(pivot Item) *Iterator  { return t.ascendF(filterBelow(pivot)) }
+func (t *LLRB) DescendBelow(pivot Item) *Iterator { return t.descendF(filterBelow(pivot)) }
+
+func (t *LLRB) AscendAtOrBelow(pivot Item) *Iterator  { return t.ascendF(filterAtOrBelow(pivot)) }
+func (t *LLRB) DescendAtOrBelow(pivot Item) *Iterator { return t.descendF(filterAtOrBelow(pivot)) }
